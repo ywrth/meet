45a821cfc190070e9d37c40fbf25efded19b0ff9
ywrth <zuatworknow@gmail.com>
2023-09-22 14:07:13 +0200

    getCalendarEvents

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,9 +4,11 @@ const { google } = require('googleapis')
 const calendar = google.calendar('v3')
 const SCOPES = [
   'https://www.googleapis.com/auth/calendar.events.public.readonly'
-]
+],
+redirect_uris = ['https://ywrth.github.io/meet/']
+
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env
-const redirect_uris = ['https://ywrth.github.io/meet']
+
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -31,3 +33,80 @@ module.exports.getAuthURL = async () => {
     })
   }
 }
+
+module.exports.getAccessToken = async (event) => {
+// Decode authorization code extracted from the URL query
+const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+return new Promise((resolve, reject) => {
+  /**
+   *  Exchange authorization code for access token with a “callback” after the exchange,
+   *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+   */
+
+  oAuth2Client.getToken(code, (error, response) => {
+    if (error) {
+      return reject(error);
+    }
+    return resolve(response);
+  });
+})
+  .then((results) => {
+    // Respond with OAuth token 
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(results),
+    };
+  })
+  .catch((error) => {
+    // Handle error
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: 'startTime',
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
