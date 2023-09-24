43352695e1ff59eef90d76446f2f4dce3cf43bcb
ywrth <zuatworknow@gmail.com>
2023-09-24 18:03:04 +0200

    Scenario 2

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,11 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
+import { extractLocations, getEvents } from '../api';
 
 describe('<CitySearch /> component', () => {
-  test('renders text input', () => {
-    const CitySearchComponent = render(<CitySearch />);
+  let CitySearchComponent;
+  beforeEach(() => {
+    CitySearchComponent = render(<CitySearch />);
+  });
+  test('updates list of suggestions correctly when user types in city textbox', async () => {
+    const user = userEvent.setup();
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+
+    // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    expect(cityTextBox).toBeInTheDocument();
-    expect(cityTextBox).toHaveClass('city');
+    await user.type(cityTextBox, "Berlin");
+
+    // filter allLocations to locations matching "Berlin"
+    const suggestions = allLocations? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+    }): [];
+
+    // get all <li> elements inside the suggestion list
+    const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+    expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+    }
   });
 });
